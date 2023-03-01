dc99ffccf95d75dbbe7ce56c77bd25a7bd558a01
Alex R <ruanalex11235@gmail.com>
2023-03-01 17:19:29 -0500

    Lab 2 : Checkpoint 1

--- Lab_2/src/App.jsx
+++ Lab_2/src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <div className="App">
       <div className='header'>
-        <h1>Simosa Selector</h1>
+        <h1>Samosa Selector</h1>
         <h2>Count: </h2>
-        <img src="https://i0.wp.com/thetwincookingproject.net/wp-content/uploads/2020/10/Sweet-Potato-Mini-Samosas3.jpg?fit=1400%2C2100&ssl=1" className='samosa'/>
+        <img className='samosa' src="https://i0.wp.com/thetwincookingproject.net/wp-content/uploads/2020/10/Sweet-Potato-Mini-Samosas3.jpg?fit=1400%2C2100&ssl=1" />
       </div>
     </div>
   )
