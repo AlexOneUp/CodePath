0333a5d0b3c57091cd8e8150b45a6d88423f8147
Alex R <ruanalex11235@gmail.com>
2023-03-01 16:51:30 -0500

    Lab 2 10% finish

--- Lab_2/src/App.jsx
+++ Lab_2/src/App.jsx
@@ -1,32 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header'>
+        <h1>Simosa Selector</h1>
+        <h2>Count: </h2>
+        <img src="https://i0.wp.com/thetwincookingproject.net/wp-content/uploads/2020/10/Sweet-Potato-Mini-Samosas3.jpg?fit=1400%2C2100&ssl=1" className='samosa'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
