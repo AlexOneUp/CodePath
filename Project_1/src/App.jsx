0039b482bdc760b37fc1dac6e58cf98cc68fb5b1
Alex R <ruanalex11235@gmail.com>
2023-02-28 21:44:26 -0500

    Lab 2 Start

--- Project_1/src/App.jsx
+++ Project_1/src/App.jsx
@@ -1,26 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Card from './components/Card';
-import images_seed from './assets/images_seed.json';
 
 const App = () => {
 
   return (
     <div className="App">
-      <Navbar />
-      <div className='container'>
-        <div className='card-container'>
-          {/* Does not work on back button routing. */}
-          {images_seed.map((image) => ( 
-          <Card 
-            source_props={image.url} 
-            description_props={image.description} 
-            hyperlink_props={image.url}
-            /> 
-            ))
-          }
-        </div>
-      </div>
+
     </div>
   )
 }
