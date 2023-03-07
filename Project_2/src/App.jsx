71be6790302832f7e4e7ca2efb834ec1eda277b6
Alex R <ruanalex11235@gmail.com>
2023-03-07 01:43:22 -0500

    Project 2 : Finished
    Added Navbar, States and Props, and more parameters to the seed file.
    Challenges included passing props and state from parent to child and vice versa.

--- Project_2/src/App.jsx
+++ Project_2/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react'
 import './App.css'
 import Flashcard from './components/Flashcard';
-
+import Navbar from './components/Navbar';
 import card_seed from './components/card_seed.json';
 
 
@@ -11,54 +11,66 @@ const App = () => {
   const [index, setIndex] = useState(0);
 
   // Helper Functions for Next and Previous Buttons
+  let callStack = [];
 
-  const increaseIndex = () => {
+  const nextRandomIndex = () => {
     setIndex(() => {
       // If index is at the end of the JSON, then we want to reset it to 0
-      if (index === card_seed.length - 1) {
-        return 0;
-      }
-      return index + 1;
+      // if (index === card_seed.length - 1) {
+      //   return 0;
+      // }
+      let random = Math.floor(Math.random() * card_seed.length);
+      callStack.push(random);
+      return random;
+      
     });
   }
 
-  const decreaseIndex = () => {
+  const previousIndex = () => {
     setIndex(() => {
+      if (index === 0) {
+        return Math.floor(Math.random() * card_seed.length);
+      }
       return index - 1;
     });
   }
 
-  const resetIndex = () => {
-    setIndex( () => {
-      return 0;
-    })
-  }
   
 
   return (
     <div className="App">
-      <div className="card-container">
-        <Flashcard 
-          question_props={card_seed[index].question} 
-          answer_props={card_seed[index].answer} 
-          />
-        {/* 
-        Don't map, but make a dictionary instead of map 
-        Want to access 1 element at a time and render it 
-        {card_seed.map((flashcard) => (
-          <Flashcard
-            question_props={flashcard.question}
-            answer_props={flashcard.answer}
-          />
-          )
-         )
-        } */}
+      {/* Header */}
+      <div className="header-container">
+        <Navbar />
       </div>
-      <div className="button-container">
-        <button className="previous-button" onClick={decreaseIndex}>Previous</button>
-        <button className="next-button" onClick={increaseIndex}>Next</button>
-        <button className="reset-button" onClick={resetIndex}>Reset</button>
-      </div>   
+
+
+      {/* Container with Content */}
+      <div className="content-container">
+        <div className="card">
+          <Flashcard 
+            question_props={card_seed[index].question} 
+            answer_props={card_seed[index].answer}
+            difficultyProps={card_seed[index].difficulty}
+            image_props={card_seed[index].image}
+            />
+          {/* 
+          Don't map, but make a dictionary instead of map 
+          Want to access 1 element at a time and render it 
+          {card_seed.map((flashcard) => (
+            <Flashcard
+              question_props={flashcard.question}
+              answer_props={flashcard.answer}
+            />
+            )
+          )
+          } */}
+        </div>
+        <div className="button-container">
+          <button className="previous-button" onClick={previousIndex}>Back</button>
+          <button className="next-button" onClick={nextRandomIndex}>Next</button>
+        </div>   
+      </div>
     </div>
   );
       
---
+++ Project_2/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/Navbar.css';
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+
+                <img src="../src/assets/logo.png" height={60}></img>
+
+            
+        </div>
+    );
+};
+
+export default Navbar;
