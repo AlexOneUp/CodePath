5f9e03cf49bea26644a6f3ecfdec6806a6f0cc7a
Alex R <ruanalex11235@gmail.com>
2023-03-21 17:29:56 -0400

    Project 4 : 1st Draft

--- Project_4/src/App.jsx
+++ Project_4/src/App.jsx
@@ -3,19 +3,16 @@ import './App.css'
 import APIForm from './Components/APIform';
 import Gallery from './Components/gallery';
 
-const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY
+const ACCESS_KEY = import.meta.env.VITE_NASA_ACCESS_KEY
 
 function App() {
   // ______________________________________________________
   // All states are stored here
   // ______________________________________________________
   const [inputs, setInputs] = useState({
-    url: "",
-    format: "",
-    no_ads: "",
-    no_cookie_banners: "",
-    width: "",
-    height: "",
+    rover : "",
+    sol : "",
+    camera : ""
   });
   
   // Checkpoint 1
@@ -29,36 +26,36 @@ function App() {
   // All functions are stored here
   // ______________________________________________________
   const submitForm = () => {
-    
-    let defaultValues = {
-      format: "jpeg",
-      no_ads: "true",
-      no_cookie_banners: "true",
-      width: "1920",
-      height: "1080",
-    };
     if (inputs.url == "" || inputs.url == " ") {
       alert("You forgot to submit an url!");
     }
     else 
     {
-      for (const [key, value] of Object.entries(inputs)) {
-        if (value == ""){
-          inputs[key] = defaultValues[key];
-        }
-      }
+      // for (const [key, value] of Object.entries(inputs)) {
+      //   if (value == ""){
+      //     inputs[key] = defaultValues[key];
+      //   }
+      // }
     }
     makeQuery();
   }
   
 
   const makeQuery = () => {
-    let wait_until = "network_idle";
-    let response_type = "json";
-    let fail_on_status = "400%2C404%2C500-511";
-    let url_starter = "https://";
-    let fullURL = url_starter + inputs.url;
-    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+    let camera = ["FHAZ", "RHAZ"]
+
+    let query = `https://api.nasa.gov/mars-photos/api/v1/rovers/${inputs.rover}/photos?sol=${inputs.sol}&api_key=${ACCESS_KEY}`;
+    if (camera.includes(inputs.camera)){
+      query = `https://api.nasa.gov/mars-photos/api/v1/rovers/${inputs.rover}/photos?sol=${inputs.sol}&camera=${inputs.camera.toLowerCase()}&api_key=${ACCESS_KEY}`
+    } else if (inputs.camera == "" && inputs.rover == "" && inputs.sol == ""){
+      query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${ACCESS_KEY}`;
+    } else if (inputs.camera == "" && inputs.rover == ""){
+      query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${ACCESS_KEY}`;
+    } else {
+      query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${ACCESS_KEY}`;
+    }
+
+    console.log(query);
     callAPI(query).catch(console.error);
   }
 
@@ -67,32 +64,27 @@ function App() {
     
     const response = await fetch(query);
     const json = await response.json();
-    if (json.url == null){
-      alert("Oops! Something went wrong with that query, let's try again!")
-        }
-        else {
-          setCurrentImage(json.url);
-          setPrevImages((images) => [...images, json.url]);
-          reset();
-        }
+    console.log(json);
+    let random = Math.floor(Math.random() * json.photos.length);
+    setCurrentImage(json.photos[random].img_src);
+    setPrevImages((images) => [...images, json.photos[random].img_src]);
+    reset();
+
   }
 
   const reset = () => {
     setInputs({
-      url: "",
-      format: "",
-      no_ads: "",
-      no_cookie_banners: "",
-      width: "",
-      height: "",
+      rover : "",
+      sol : "",
+      camera : ""
     });
     
   }
 
     return (
     <div className="whole-page">
-      <h1>Grab It! </h1>
-      
+      <h1>Mars Rover Picture Generator </h1>
+      <h2>Randomly generate a photo or fill out the form!</h2>
       <APIForm
         inputs={inputs}
         handleChange={(e) =>
@@ -117,17 +109,11 @@ function App() {
     <div className="container">
       <h3> Current Query Status: </h3>
       <p>
-        https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY    
+        https://api.nasa.gov/mars-photos/api/v1/rovers/{inputs.rover}/photos?&api_key=ACCESS_KEY 
         <br></br>
-        &url={inputs.url} <br></br>
-        &format={inputs.format} <br></br>
-        &width={inputs.width}
-        <br></br>
-        &height={inputs.height}
-        <br></br>
-        &no_cookie_banners={inputs.no_cookie_banners}
-        <br></br>
-        &no_ads={inputs.no_ads}
+        &rover={inputs.rover} <br></br>
+        &fsol={inputs.sol} <br></br>
+        &camera={inputs.camera}
         <br></br>
       </p>
     </div>
--- Project_4/src/components/APIform.jsx
+++ Project_4/src/components/APIform.jsx
@@ -3,12 +3,9 @@ import React from "react";
 const APIForm = ({inputs, handleChange, onSubmit}) => {
 
     const inputsInfo = [
-        "Input a link to any website you would like to take a screenshot of. Do not include https or any protocol in the URL",
-        "Input which image format you would prefer for your screenshot: jpeg, png, or webp",
-        "Input true or false if you would like your website screenshot to not contain any ads",
-        "Input true or false if you would like your website screenshot to not contain of those annoying 'allow cookies' banners",
-        "Choose the width of your screenshot (in pixels)",
-        "Choose the height of your screenshot (in pixels)",
+        "Input a Martian Rover name: curiosity, opportunity, or spirit",
+        "Input sol (Martian day) of the rover's mission (recommend 1000)",
+        "Input Camera the rover used to take the photo: FHAZ or RHAZ"
       ];
 
 
@@ -37,7 +34,7 @@ const APIForm = ({inputs, handleChange, onSubmit}) => {
             ))}
         </form>
         <button type="submit" className="button" onClick={onSubmit}>
-        Take that Pic! 🎞
+        Get me a picture Rover! 🎞
         </button>
       </div>
     );
